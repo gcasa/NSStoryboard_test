eca459de06d614ca1f6200506958843a3e878dcb
Gregory Casamento <greg.casamento@gmail.com>
2020-06-22 09:44:43 -0400

    Test application subclass

---
+++ NSStoryboard_test/ApplicationSubclass.h
@@ -0,0 +1,17 @@
+//
+//  ApplicationSubclass.h
+//  NSStoryboard_test
+//
+//  Created by Gregory Casamento on 6/22/20.
+//  Copyright © 2020 Open Logic. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ApplicationSubclass : NSApplication
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ NSStoryboard_test/ApplicationSubclass.m
@@ -0,0 +1,13 @@
+//
+//  ApplicationSubclass.m
+//  NSStoryboard_test
+//
+//  Created by Gregory Casamento on 6/22/20.
+//  Copyright © 2020 Open Logic. All rights reserved.
+//
+
+#import "ApplicationSubclass.h"
+
+@implementation ApplicationSubclass
+
+@end
